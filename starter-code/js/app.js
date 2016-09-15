b5ae7793a128028a48b3673c176e5f92cf1b802f
Sherie Carter <sherie1.carter@gmail.com>
2016-09-15 09:05:52 -0700

    1-5

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,3 +2,17 @@ console.log('app.js loaded!');
 
 // initialize the application
 var app = angular.module("hangmanApp", []);
+app.controller("hangmanCtrl", hangmanCtrl);
+
+function hangmanCtrl(){
+  var vm= this;
+ this.game=new HangmanGame('elephant');
+
+ vm.guessLetter = function() {
+   // guess the letter
+vm.game.guess(vm.userInput)
+
+   // clear the input
+   vm.userInput="";
+ }
+}
